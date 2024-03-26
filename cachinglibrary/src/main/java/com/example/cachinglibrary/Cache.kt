96c1e52ff393960e902a36091b568f75513c1824
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-03-26 13:19:54 +0530

    [003]: Changed Cache from class to object and provided explicit visibility modifiers

--- cachinglibrary/src/main/java/com/example/cachinglibrary/Cache.kt
+++ cachinglibrary/src/main/java/com/example/cachinglibrary/Cache.kt
@@ -1,13 +1,13 @@
 package com.example.cachinglibrary
 
-class Cache {
+public object Cache {
 
     private val cache by lazy { mutableMapOf<String, Any>() }
 
-    fun setData(key: String, data: Any) {
+    public fun setData(key: String, data: Any) {
         cache[key] = data
     }
 
-    fun getData(key: String): Any? = cache[key]
+    public fun getData(key: String): Any? = cache[key]
 
 }
