3bacd0571e7ef0e57d3d2786a979ae2deb8b2d50
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-03-27 06:15:56 +0530

    [005]: Added an activity in app module to test the caching library

---
+++ app/src/main/java/com/example/androidcachinglibrary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidcachinglibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cachinglibrary.Cache
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
